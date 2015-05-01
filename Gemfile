de32e564473b81970e6380fd9b28f0742d6ea8c9
Reid Beels <mail@reidbeels.com>
2015-05-01 15:20:37 -0700

    Enable live_reload and directory_indexes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem "middleman", "~> 3.3.11"
 gem "middleman-blog", "~> 3.5.3"
+gem "middleman-livereload"
 
 # gem "middleman-syntax"
 # gem "middleman-automatic-clowncar"
--- config.rb
+++ config.rb
@@ -20,6 +20,8 @@ end
 
 page "/feed.xml", layout: false
 
+activate :directory_indexes
+
 ###
 # Compass
 ###
@@ -58,7 +60,7 @@ page "/feed.xml", layout: false
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
